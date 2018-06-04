3ac672ee15758a730e62f6e9dca9f08f885071c9
wangld5 <wangld4195@163.com>
2018-06-04 15:29:40 +0800

    packageSystem

---
+++ Assets/ActionManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+
+namespace Game_Manager {
+
+	public class Game_Scene_Manager : System.Object {
+		private static Game_Scene_Manager _instance;
+		private static mouse_image _Mouse;
+
+		public static Game_Scene_Manager GetInstance() {
+			if (_instance == null) {
+				_instance = new Game_Scene_Manager();
+			}
+			return _instance;
+		}
+
+		public void SetMouse(mouse_image _mouse) {
+			if (_Mouse == null) {
+				_Mouse = _mouse;
+			}
+		}
+
+		public mouse_image GetMouse() {
+			return _Mouse;
+		}
+	}
+
+}
---
+++ Assets/mouse_image.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Game_Manager;
+
+public class mouse_image : MonoBehaviour {
+
+	// Use this for initialization
+	private Game_Scene_Manager gsm;
+	private Image Mouse_image;
+	private int mouse_type = 0;
+	public Sprite none;
+	public Sprite head;
+	public Sprite arm;
+	public Sprite shoes;
+	public Color None;
+	public Color NotNone;
+	public Camera cam;
+
+	void Awake() {
+		gsm = Game_Scene_Manager.GetInstance();
+		gsm.SetMouse(this);
+		Mouse_image = GetComponent<Image>();
+	}
+
+	public int GetMouseType() {
+		return mouse_type;
+	}
+
+	public void SetMouseType(int Mouse_type) {
+		mouse_type = Mouse_type;
+	}
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		// Debug.Log(mouse_type);
+		if (mouse_type == 0) {
+			Mouse_image.sprite = none;
+			Mouse_image.color = None;
+		} else {
+			Mouse_image.color = NotNone;
+			if (mouse_type == 1)
+				Mouse_image.sprite = head;
+			else if (mouse_type == 2)
+				Mouse_image.sprite = arm;
+			else if (mouse_type == 3)
+				Mouse_image.sprite = shoes;
+			Vector3 mp = Input.mousePosition;
+			Vector3 mmp = cam.ScreenToWorldPoint(mp + new Vector3(0, 0, 350));
+			//transform.position = new Vector3(mp.x - 450, mp.y - 200, 0);
+			transform.position = new Vector3(mmp.x, mmp.y, 0);
+
+			// Debug.Log("mp: " + mp);
+			// Debug.Log("mmp: " + mmp);
+		}
+
+	}
+}
